54ff449af527ab9d6c26b84ee872de28799f84a8
Sofi-Coffee <sofia.davidova999@gmail.com>
2021-05-08 17:51:19 +0300

    new commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,46 +1,59 @@
 import '../scss/style.scss';
+import './form';
 
 const elemRu = document.getElementsByClassName('ruSection');
 const elemEng = document.getElementsByClassName('engSection');
 const elemAr = document.getElementsByClassName('arSection');
+
+function changeLanguage(event,language){
+  event.preventDefault();
+  if(language=='ru'){
+    for (let i = 0; i < elemRu.length; i++) {
+      elemRu[i].classList.remove('ruLng');
+      elemEng[i].classList.add('engLng');
+      elemAr[i].classList.add('arLng');
+    }
+  }
+  else if(language=='eng'){
+    for (let i = 0; i < elemEng.length; i++) {
+      elemRu[i].classList.add('ruLng');
+      elemEng[i].classList.remove('engLng');
+      elemAr[i].classList.add('arLng');
+    }
+  }
+  else if(language=='ar'){
+    for (let i = 0; i < elemAr.length; i++) {
+      elemRu[i].classList.add('ruLng');
+      elemEng[i].classList.add('engLng');
+      elemAr[i].classList.remove('arLng');
+    }
+  }
+}
 function btnRuSlider(event) {
   event.preventDefault();
-  for (let i = 0; i < elemRu.length; i++) {
-    elemRu[i].classList.remove('ru');
-    elemEng[i].classList.add('eng');
-    elemAr[i].classList.add('ar');
-  }
-
+  changeLanguage(event,"ru");
 }
 function btnEngSlider(event) {
   event.preventDefault();
-  for (let i = 0; i < elemEng.length; i++) {
-    elemEng[i].classList.remove('eng');
-    elemRu[i].classList.add('ru');
-    elemAr[i].classList.add('ar');
-  }
+  changeLanguage(event,"eng");
 }
 function btnArSlider(event) {
   event.preventDefault();
-  for (let i = 0; i < elemAr.length; i++) {
-    elemAr[i].classList.remove('ar');
-    elemEng[i].classList.add('eng');
-    elemRu[i].classList.add('ru');
-  }
+  changeLanguage(event,"ar");
 }
 
-
 window.onload = function () {
-  const btnRu = document.getElementsByClassName('header__btnRU');
+  const btnRu = document.getElementsByClassName('btnRu');
   for (let i = 0; i < btnRu.length; i++) {
     btnRu[i].addEventListener("click", btnRuSlider);
   }
-  const btnEng = document.getElementsByClassName('header__btnENG');
+  const btnEng = document.getElementsByClassName('btnEng  ');
   for (let i = 0; i < btnEng.length; i++) {
     btnEng[i].addEventListener("click", btnEngSlider);
   }
-  const btnAr = document.getElementsByClassName('header__btnAR');
+  const btnAr = document.getElementsByClassName('btnAr');
   for (let i = 0; i < btnAr.length; i++) {
     btnAr[i].addEventListener("click", btnArSlider);
   }
+
 }
